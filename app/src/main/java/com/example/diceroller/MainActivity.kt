6d5fd1dc90cf8cf269a3e6638991d85cf0ea2110
henrique151 <henrique.sousa60@etec.sp.gov.br>
2024-04-09 08:34:33 -0300

    V2 DiceRoller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerApp()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    Column(
+        modifier = modifier
+            .fillMaxSize(), // Ocupa todo o espaço disponível
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(getDiceImage(result)),
+            contentDescription = result.toString(),
+            modifier = Modifier
+                .height(200.dp) // Ajuste a altura conforme necessário
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = Random.nextInt(1, 7) }) {
+            Text(stringResource(R.string.roll))
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+private fun getDiceImage(result: Int): Int {
+    return when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+}
